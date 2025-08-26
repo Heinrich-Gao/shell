79c2d50ad6fec2078e8802da3bc9d3f3f83522ca
Gao-Heinrich <1851146390k@gmail.com>
2025-08-26 22:18:24 +0800

    Add files via upload

--- Day5 log-analysis/log-analysis.sh
+++ Day5 log-analysis/log-analysis.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+total=0
+
+for f in $HOME/aws_practice/*.log;do    #遍历指定目录下的所有.log文件
+    count=$(grep -ci "ERROR" "$f")
+    echo "$count $f"
+    total=$((total += count))    #累加至计数器
+done | sort -nr    #将输出按照数字的降序排序
+echo "---------------------"
+echo "TOTAL ERRORS : $total"    #输出总错误数
