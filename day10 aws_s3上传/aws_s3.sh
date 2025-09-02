e4ca39a79c03bfc34d50e8c879d7af79646e8074
Gao-Heinrich <1851146390k@gmail.com>
2025-09-02 18:04:40 +0800

    Add files via upload

--- day10 aws_s3上传/aws_s3.sh
+++ day10 aws_s3上传/aws_s3.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+#遍历目标目录并将文件逐个上传至S3桶
+DIR="$HOME/aws_practice/study-2"    #根据实际路径随时更改
+BUCKET="heinrich-study-bucket-1"    #根据实际桶名随时更改
 
+#遍历 "$DIR" 中的所有内容
+for f in "$DIR"/*;do
+    #将检索到的文件上传至S3桶
+    aws s3 cp "$f" s3://$BUCKET/
+    echo "上传完成：$(basename "$f")"
+done
