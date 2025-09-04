018fe3e02e86e4ab57fca5b05cfae65cb82e6e8b
Gao-Heinrich <1851146390k@gmail.com>
2025-09-04 15:59:42 +0800

    Update manage_ec2.sh

--- day11 manage_ec2/manage_ec2.sh
+++ day11 manage_ec2/manage_ec2.sh
@@ -6,8 +6,8 @@
 INSTANCE_NAME=$1
 #使用AWS CLI 查询实例ID
 INSTANCE_ID=$(aws ec2 describe-instances \
---filters "Name=tag:Name,Values=$INSTANCE_NAME" \    #按照标签Name 筛选实例
---query "Reservations[].Instances[].InstanceId" \    #只提取 InstanceId
+--filters "Name=tag:Name,Values=$INSTANCE_NAME" \    
+--query "Reservations[].Instances[].InstanceId" \    
 --output text)
 #指定要使用的 AMI ID
 AMI_ID="ami-096566f39a31a283e"
@@ -19,11 +19,11 @@ else
    echo "未找到名为 $INSTANCE_NAME 的实例"
    #如果不存在则使用AWS CLI 创建实例
    aws ec2 run-instances \
-  --image-id "$AMI_ID" \        #指定AMI
-  --count 1 \                   #创建1个实例
-  --instance-type t3.micro \    #指定实例类型
-  --key-name my-key \           #指定SSH 密钥对
-  --security-group-ids sg-0deb2241624b13fee \    #安全组 ID
+  --image-id "$AMI_ID" \       
+  --count 1 \                   
+  --instance-type t3.micro \    
+  --key-name my-key \           
+  --security-group-ids sg-0deb2241624b13fee \   
   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]"    #给实例打上 Name 标签，便于管理
     echo "已创建名为 $INSTANCE_NAME 的实例"
 fi
